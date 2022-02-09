e875e553debf505232455cd8cfd3194097fc759d
Dominik <poczta@dominikmaciag.com>
2022-02-09 12:36:17 +0100

    *ngIf, ng-container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
 import { NgForComponent } from './ng-for/ng-for.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
-    NgForComponent
+    NgForComponent,
+    NgIfComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ng-if/ng-if.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-if',
+  templateUrl: './ng-if.component.html',
+  styleUrls: ['./ng-if.component.css']
+})
+export class NgIfComponent implements OnInit {
+
+  constructor() {
+    this.dogs.push(new Dog('Java', 4), new Dog('Push', 6));
+  }
+
+  ngOnInit(): void {
+  }
+  dogs = new Array<Dog>();
+  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+}
+
+class Dog {
+  constructor(public name: string, public age: number) {
+
+  }
+}
