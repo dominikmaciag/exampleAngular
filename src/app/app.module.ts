da502102e1e003301c997b70368fc98bf6c7d749
Dominik <poczta@dominikmaciag.com>
2022-02-09 13:11:55 +0100

    ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
     NgForComponent,
-    NgIfComponent
+    NgIfComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
